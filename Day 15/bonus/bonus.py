81ec071725b6cf8b4503c893dcba3359c72f4c8d
Anupam Das <98011322+ItsMrAD@users.noreply.github.com>
2023-05-15 01:06:29 +0530

    Day 14: Optimized the code,added modules

--- Day 15/bonus/bonus.py
+++ Day 15/bonus/bonus.py
@@ -5,15 +5,21 @@ with open("questions.json", "r") as file:
 
 data = json.loads(content)
 
-score = 0
 for question in data:
     print(question["question_text"])
     for index, alternative in enumerate(question["alternatives"]):
         print(index + 1, "-", alternative)
     user_choice = int(input("Enter your answer: "))
     question["user_choice"] = user_choice
-    if question["user_choice"] == question["correct answer"]:
-        score = score + 1
 
-print(data)
-print(score)
+score = 0
+for index, question in enumerate(data):
+    if question["user_choice"] == question["correct_answer"]:
+        score = score + 1
+        result = "Correct Answer"
+    else:
+        result = "Wrong Answer"
+    message = f"{index + 1}: {result} - Your answer: {question['user_choice']}, " \
+              f"Correct answer: {question['correct_answer']}"
+    print(message)
+print(score, "/", len(data))
---
+++ Day 15/exe.py
@@ -0,0 +1,9 @@
+import random
+
+lb = int(input("Enter the lower bound: "))
+ub = int(input("Enter the upper bound: "))
+
+number = random.randrange(lb, ub)
+number2 = random.randint(lb, ub)
+
+print(number)
