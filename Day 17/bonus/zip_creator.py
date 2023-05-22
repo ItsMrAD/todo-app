72aee0218fe442958847669e72114a1bdc024eb3
Anupam Das <98011322+ItsMrAD@users.noreply.github.com>
2023-05-23 04:07:48 +0530

    Day 17 - bonus project complete

--- Day 17/bonus/zip_creator.py
+++ Day 17/bonus/zip_creator.py
@@ -6,6 +6,7 @@ def make_archive(filepaths, dest_dir):
     dest_path = pathlib.Path(dest_dir, "compressed.zip")
     with zipfile.ZipFile(dest_path, 'w') as archive:
         for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
             archive.write(filepath, arcname=filepath.name)
 
 
